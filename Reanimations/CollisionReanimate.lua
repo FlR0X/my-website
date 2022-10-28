aaedae399dc73fb25db1d7e5e251ebbadf770a60
Firox <86130692+Firoxus@users.noreply.github.com>
2022-10-28 13:12:17 +0200

    Update CollisionReanimate.lua

--- Reanimations/CollisionReanimate.lua
+++ Reanimations/CollisionReanimate.lua
@@ -68,7 +68,7 @@ local humState16 = true --enables collisions for limbs before the humanoid dies 
 local addtools = false --puts all tools from backpack to character and lets you hold them after reanimation
 local hedafterneck = true --disable aligns for head and enable after neck or torso is removed
 local loadtime = game:GetService("Players").RespawnTime + 0.5 --anti respawn delay
-local method = 1 --reanimation method
+local method = 0 --reanimation method
 --methods:
 --0 - breakJoints (takes [loadtime] seconds to load)
 --1 - limbs
